b994b67a1efda772cfd884656cd83a75eb5f5481
Nicholas Hoyt <nhoyt@illinois.edu>
2016-06-30 11:05:45 -0500

    Initial commit

---
+++ MenuItem.js
@@ -0,0 +1,130 @@
+/**
+*   @constructor MenuItem
+*
+*   @desc
+*       Wrapper object for a simple menu item in a popup menu
+*
+*   @param domNode
+*       The DOM element node that serves as the menu item container.
+*       The menuObj PopupMenu is responsible for checking that it has
+*       requisite metadata, e.g. role="menuitem".
+*
+*   @param menuObj
+*       The object that is a wrapper for the PopupMenu DOM element that
+*       contains the menu item DOM element.
+*/
+var MenuItem = function (domNode, menuObj) {
+
+  this.domNode = domNode;
+  this.menu = menuObj;
+
+  this.keyCode = Object.freeze({
+    'TAB'      :  9,
+    'RETURN'   : 13,
+    'ESC'      : 27,
+    'SPACE'    : 32,
+    'PAGEUP'   : 33,
+    'PAGEDOWN' : 34,
+    'END'      : 35,
+    'HOME'     : 36,
+    'LEFT'     : 37,
+    'UP'       : 38,
+    'RIGHT'    : 39,
+    'DOWN'     : 40
+  });
+};
+
+MenuItem.prototype.init = function () {
+  this.domNode.tabIndex = -1;
+
+  this.domNode.addEventListener('keydown', this.handleKeydown.bind(this));
+  this.domNode.addEventListener('click',   this.handleClick.bind(this));
+  this.domNode.addEventListener('focus',   this.handleFocus.bind(this));
+  this.domNode.addEventListener('blur',    this.handleBlur.bind(this));
+};
+
+// Event handlers for menuitem elements in a PopupMenu. See PopupMenu.js
+// for the definition of the object passed in as the menu parameter.
+MenuItem.prototype.handleKeydown = function (event) {
+  var tgt = event.currentTarget,
+      flag = false, clickEvent;
+
+  switch (event.keyCode) {
+    case this.keyCode.SPACE:
+    case this.keyCode.RETURN:
+      // Create simulated mouse event to mimic the behavior of ATs
+      // and let the event handler handleClick do the housekeeping.
+      try {
+        clickEvent = new MouseEvent('click', {
+          'view': window,
+          'bubbles': true,
+          'cancelable': true
+        });
+      }
+      catch(err) {
+        if (document.createEvent) {
+          // DOM Level 3 for IE 9+
+          clickEvent = document.createEvent('MouseEvents');
+          clickEvent.initEvent('click', true, true);
+        }
+      }
+      tgt.dispatchEvent(clickEvent);
+      flag = true;
+      break;
+
+    case this.keyCode.ESC:
+      this.menu.setFocusToController();
+      this.menu.close(true);
+      flag = true;
+      break;
+
+    case this.keyCode.UP:
+      this.menu.setFocusToPreviousItem(tgt);
+      flag = true;
+      break;
+
+    case this.keyCode.DOWN:
+      this.menu.setFocusToNextItem(tgt);
+      flag = true;
+      break;
+
+    case this.keyCode.HOME:
+    case this.keyCode.PAGEUP:
+      this.menu.setFocusToFirstItem();
+      flag = true;
+      break;
+
+    case this.keyCode.END:
+    case this.keyCode.PAGEDOWN:
+      this.menu.setFocusToLastItem();
+      flag = true;
+      break;
+
+    case this.keyCode.TAB:
+      this.menu.setFocusToController();
+      this.menu.close(true);
+      break;
+
+    default:
+      break;
+  }
+
+  if (flag) {
+    event.stopPropagation();
+    event.preventDefault();
+  }
+};
+
+MenuItem.prototype.handleClick = function (event) {
+  this.menu.setFocusToController();
+  this.menu.close(true);
+};
+
+MenuItem.prototype.handleFocus = function (event) {
+  this.menu.hasFocus = true;
+};
+
+MenuItem.prototype.handleBlur = function (event) {
+  this.menu.hasFocus = false;
+  setTimeout(this.menu.close.bind(this.menu, false), 300);
+};
